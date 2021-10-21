aa41674d1e14e82c1ed23df7674c73ff0d873247
David Lavanchy <dplavanchy@gmail.com>
2021-10-21 18:09:46 -0400

    finished out data entities

---
+++ ForeScore.Data/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace ForeScore.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ForeScore.Data.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "ForeScore.Data.ApplicationDbContext";
+        }
+
+        protected override void Seed(ForeScore.Data.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
