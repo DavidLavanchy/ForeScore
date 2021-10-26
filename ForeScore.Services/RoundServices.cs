a6331b968da104e1a9a1e9799433eb8a65c0e6bd
David Lavanchy <dplavanchy@gmail.com>
2021-10-25 20:31:13 -0400

    finished Round read methods and edit method

--- ForeScore.Services/RoundServices.cs
+++ ForeScore.Services/RoundServices.cs
@@ -19,7 +19,7 @@ namespace ForeScore.Services
 
         public bool CreateRound(RoundCreateModel model)
         {
-            using(var ctx = new ApplicationDbContext())
+            using (var ctx = new ApplicationDbContext())
             {
                 var entity = new Round
                 {
@@ -40,12 +40,12 @@ namespace ForeScore.Services
 
         public RoundDetail GetRoundById(int id)
         {
-            using(var ctx = new ApplicationDbContext())
+            using (var ctx = new ApplicationDbContext())
             {
                 var entity =
                     ctx
                     .Rounds
-                    .Single(e => e.RoundId == id);
+                    .Single(e => e.RoundId == id && e.Id == _userId);
 
                 var round = new RoundDetail
                 {
@@ -62,5 +62,98 @@ namespace ForeScore.Services
                 return round;
             }
         }
+
+        public ICollection<RoundListItem> GetAllRoundsByUserId()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Rounds
+                    .Where(e => e.Id == _userId)
+                    .Select(e=> 
+                new RoundListItem
+                {
+                    CourseId = e.CourseId,
+                    DateOfRound = e.DateOfRound,
+                    Description = e.Description,
+                    IsFeatured = e.IsFeatured,
+                    IsPublic = e.IsPublic,
+                    Score = e.Score,
+                });
+
+                return query.ToArray();
+            }
+        }
+
+        public ICollection<RoundListItem> GetAllRoundsByUserIdAscending()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Rounds
+                    .Where(e => e.Id == _userId)
+                    .OrderBy(e=> e.Score)
+                    .Select(e =>
+                new RoundListItem
+                {
+                    CourseId = e.CourseId,
+                    DateOfRound = e.DateOfRound,
+                    Description = e.Description,
+                    IsFeatured = e.IsFeatured,
+                    IsPublic = e.IsPublic,
+                    Score = e.Score,
+                });
+
+                return query.ToArray();
+            }
+        }
+
+        public ICollection<RoundListItem> GetAllRoundsByUserIdAndDate()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Rounds
+                    .Where(e => e.Id == _userId)
+                    .OrderBy(e => e.DateOfRound)
+                    .Select(e =>
+                new RoundListItem
+                {
+                    CourseId = e.CourseId,
+                    DateOfRound = e.DateOfRound,
+                    Description = e.Description,
+                    IsFeatured = e.IsFeatured,
+                    IsPublic = e.IsPublic,
+                    Score = e.Score,
+                });
+
+                return query.ToArray();
+            }
+        }
+
+        public bool EditRound(RoundEdit model)
+        {
+            using(var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Rounds
+                    .Single(e => model.RoundId == e.RoundId);
+
+                    entity.DateOfRound = model.DateOfRound;
+                    entity.Description = model.Description;
+                    entity.IsFeatured = model.IsFeatured;
+                    entity.IsPublic = model.IsPublic;
+                    entity.Score = model.Score;
+                    entity.HoleData = model.HoleData;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+
     }
 }
